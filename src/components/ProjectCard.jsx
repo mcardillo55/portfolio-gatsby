6f7198598efc134e4b7067224ecb0d26460b93c4
Mike Cardillo <mike@mjcardillo.com>
2020-07-28 20:42:00 -0700

    Made ProjectCards 100% height

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import Button from 'react-bootstrap/Button'
 
 const ProjectCard = (props) => {
     return(
-        <Card>
+        <Card className="h-100">
             <Card.Img variant="top" src={props.image} />
             <Card.Body>
                 <Card.Title>{props.title}</Card.Title>
